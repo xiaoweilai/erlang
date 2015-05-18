f378588b9ef22136c334703ecb75d6df09887018
yjduoduo <wxjlmr@126.com>
2015-05-18 20:16:39 +0800

    获取code2.spec里的文件名

--- daima_code/file_example.erl
+++ daima_code/file_example.erl
@@ -1,7 +1,11 @@
 %% 文件读写示例
+
 -module(file_example).
 
--export([write/2, read/1]).
+-export([write/2, read/1, readline/1]).
+
+%% -*- coding: utf-8 -*-
+%% -*- coding: latin-1 -*-
 
 %% --------------------
 %% 写入文件
@@ -24,6 +28,20 @@ write(Content, FileName) when is_list(Content); is_list(FileName) ->
 read(FileName) when is_list(FileName) ->
     {ok, Content} = file:read_file(FileName),
     Content.
+
+%% --------------------
+%% 读取传入文件名中内容对应的文件
+%% read(FileName) -> binary()
+%% FileName = string()
+%% --------------------
+readline(FileName) when is_list(FileName) ->
+	{ok, F} = file:open(FileName,read),
+	{ok, {file_name, NewFileName}}=io:read(F,''),
+	%%{file_name, _} = io:read(F,''),
+	file:close(F),
+	%%io:format("~s~n", [NewFileName]),
+	{ok,NewFileName}.
+
 
 get_local_path() ->
     filename:dirname(code:which(?MODULE)).
